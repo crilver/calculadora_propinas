37c339317fecf15f427bddb8d3c10df5aa48b731
crilver <cris_99@hotmail.es>
2024-05-01 22:00:52 -0500

    Finalización calculadora propinas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import MenuItem from "./components/MenuItem"
 import OrderContents from "./components/OrderContents"
+import OrderTotals from "./components/OrderTotals"
+import TipPercentageForm from "./components/TipPercentageForm"
 import { menuItems } from "./data/db"
 import useOrder from "./hooks/useOrder"
 
 function App() {
 
-  const { order, addItem, removeItem } = useOrder()
+  const { order, tip, setTip, addItem, removeItem, placeOrder } = useOrder()
 
   return (
     <>
@@ -27,10 +29,25 @@ function App() {
           </div>
         </div>
         <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
-          <OrderContents
-            order = {order}
-            removeItem = {removeItem}
-          />
+          {order.length ? (
+            <>
+              <OrderContents
+                order = {order}
+                removeItem = {removeItem}
+              />
+              <TipPercentageForm 
+                setTip = {setTip}
+                tip = {tip}
+              />
+              <OrderTotals 
+                order = {order}
+                tip = {tip}
+                placeOrder = {placeOrder}
+              />
+            </>
+          ) : (
+            <p className="text-center">La orden está vacia</p>
+          ) }          
         </div>
       </main>
     </>
